e4a00f3dfad276db9b62f1083ca2f77e1a369e5e
Carlos Escobedo <carlosdev1810@gmail.com>
2024-04-12 15:17:49 -0600

    emails_cmdb: Added routing and API handlers for conecting to the database

--- server/src/controllers/emails_cmdb.controllers.js
+++ server/src/controllers/emails_cmdb.controllers.js
@@ -1,8 +1,114 @@
 import { pool } from '../db.js'
 
+// areas emails
+
+export const addArea = async({ body }, res) => {
+  
+  const { area } = body
+  const query = 'INSERT INTO areas_emails_cmdb (`area`) VALUES (?)'
+
+  try {
+    await pool.query(query, [area])
+    res.status(200).send('Information uploaded correctly.')
+  } catch (error) {
+    res.status(400).send('There was an error trying to load the information.')
+  }
+
+}
+
 export const getAreas = async(req, res) => {
 
-  const [result] = await pool.query('SELECT * FROM areas_emails_cmdb')
-  res.send(result)
+  const query = 'SELECT * FROM areas_emails_cmdb'
+
+  try {
+    const [result] = await pool.query(query)
+    res.status(200).json(result)
+  } catch (error) {
+    res.status(400).send('Error when trying to obtain the information.')
+  }
+
+}
+
+export const updateArea = async({ body }, res) => {
+  
+  const { id_area, area } = body
+  const query = 'UPDATE areas_emails_cmdb SET `area` = ? WHERE id_area = ?'
+
+  try {
+    await pool.query(query, [area, id_area])
+    res.status(200).send('Information was updated correctly.')
+  } catch (error) {
+    res.status(400).send('There was an error trying to update the information.')
+  }
+
+}
+
+export const deleteArea = async(req, res) => {
+
+  const { id_area } = req.query
+  const query = 'DELETE FROM areas_emails_cmdb WHERE id_area = ?'
+  
+  try {
+    await pool.query(query, [id_area])
+    res.status(200).send('Information successfully deleted.')
+  } catch (error) {
+    res.status(200).send('There was an error trying to delete the information.')
+  }
+
+}
+
+// registers emails
+
+export const getRegistersByArea = async(req, res) => {
+
+  const { id_area } = req.query
+  const query = 'SELECT * FROM registers_emails_cmdb WHERE id_area = ?'
+
+  try {
+    const [result] = await pool.query(query, [id_area])
+    res.status(200).json(result)
+  } catch (error) {
+    res.status(400).send('Error when trying to obtain the information.')
+  }
+
+}
+
+export const addRegisterByArea = async({ body }, res) => {
+  const { name, email, password, id_area } = body
+  const query = 'INSERT INTO registers_emails_cmdb (`name`, `email`, `password`, `id_area`) VALUES (?,?,?,?)'
+  
+  try {
+    await pool.query(query, [name, email, password, id_area])
+    res.status(200).send('Information added correctly.')
+  } catch (error) {
+    res.status(400).send('There was an error trying to add the information.')
+  }
+}
+
+export const updateRegisterByArea = async({ body }, res) => {
+  
+  const { id_register, name, email, password } = body
+  const query = 'UPDATE registers_emails_cmdb SET `name` = ?, `email` = ?, `password` = ? WHERE id_register = ?'
+
+  try {
+    await pool.query(query, [name, email, password, id_register])
+    res.status(200).send('Information was updated correctly.')
+  } catch (error) {
+    res.status(400).send('There was an error trying to update the information.')
+  }
+
+}
+
+export const deleteRegisterByArea = async(req, res) => {
+
+  const { id_register } = req.query
+  const query = 'DELETE FROM registers_emails_cmdb WHERE id_register = ?'
+  
+  try {
+    await pool.query(query, [id_register])
+    res.status(200).send('Information successfully deleted.')
+  } catch (error) {
+    res.status(200).send('There was an error trying to delete the information.')
+  }
 
 }
--- server/src/routes/emails_cmdb.routes.js
+++ server/src/routes/emails_cmdb.routes.js
@@ -1,9 +1,26 @@
 import { Router } from 'express'
 
-import { getAreas } from '../controllers/emails_cmdb.controllers.js'
+import { 
+  addArea, 
+  addRegisterByArea, 
+  deleteArea, 
+  deleteRegisterByArea, 
+  getAreas, 
+  getRegistersByArea, 
+  updateArea,
+  updateRegisterByArea
+} from '../controllers/emails_cmdb.controllers.js'
 
 const router = Router()
 
 router.get('/cmdb/emails/areas', getAreas)
+router.post('/cmdb/emails/areas', addArea)
+router.put('/cmdb/emails/areas', updateArea)
+router.delete('/cmdb/emails/areas', deleteArea)
+
+router.get('/cmdb/emails/registers', getRegistersByArea)
+router.post('/cmdb/emails/registers', addRegisterByArea)
+router.put('/cmdb/emails/registers', updateRegisterByArea)
+router.delete('/cmdb/emails/registers', deleteRegisterByArea)
 
 export default router
