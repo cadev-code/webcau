1fd8896e7846d2e92be493d0ae679df5b5dd907c
Carlos Escobedo <carlosdev1810@gmail.com>
2024-05-15 22:56:28 -0600

    UPDATE emails_cmdb: all query methods fot lists_emails_cmdb table were added to the server

--- server/src/controllers/emails_cmdb.controllers.js
+++ server/src/controllers/emails_cmdb.controllers.js
@@ -57,6 +57,63 @@ export const deleteArea = async(req, res) => {
 
 }
 
+// lists emails
+
+export const addList = async({ body }, res) => {
+  
+  const { text } = body
+  const query = 'INSERT INTO lists_emails_cmdb (`list`) VALUES (?)'
+
+  try {
+    await pool.query(query, [text])
+    res.status(200).send('Information uploaded correctly.')
+  } catch (error) {
+    res.status(400).send('There was an error trying to load the information.')
+  }
+
+}
+
+export const getLists = async(req, res) => {
+
+  const query = 'SELECT * FROM lists_emails_cmdb ORDER BY `lists` ASC'
+
+  try {
+    const [result] = await pool.query(query)
+    res.status(200).json(result)
+  } catch (error) {
+    res.status(400).send('Error when trying to obtain the information.')
+  }
+
+}
+
+export const updateList = async({ body }, res) => {
+  
+  const { id, text } = body
+  const query = 'UPDATE lists_emails_cmdb SET `list` = ? WHERE id_list = ?'
+
+  try {
+    await pool.query(query, [text, id])
+    res.status(200).send('Information was updated correctly.')
+  } catch (error) {
+    res.status(400).send('There was an error trying to update the information.')
+  }
+
+}
+
+export const deleteList = async(req, res) => {
+
+  const { id_list } = req.query
+  const query = 'DELETE FROM lists_emails_cmdb WHERE id_list = ?'
+  
+  try {
+    await pool.query(query, [id_list])
+    res.status(200).send('Information successfully deleted.')
+  } catch (error) {
+    res.status(200).send('There was an error trying to delete the information.')
+  }
+
+}
+
 // registers emails
 
 // get id_area for add or update register
