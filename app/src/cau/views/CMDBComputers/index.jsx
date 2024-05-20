bde83652037b8d0522a1178ec20e79940e25fc04
Carlos Escobedo <carlosdev1810@gmail.com>
2024-05-20 15:36:04 -0600

    UPDATE computers_cmdb: TitleActionBar component is added

--- app/src/cau/views/CMDBComputers/index.jsx
+++ app/src/cau/views/CMDBComputers/index.jsx
@@ -1,12 +1,27 @@
+import { TitleActionBar } from '../../components/TitleActionBar'
 
 // import { dataToExcel } from '../../helpers/dataToExcel'
-
-import { TitleActionBar } from "../../components/TitleActionBar"
 
 export const CMDBComputers = ({ userData }) => {
 
   return (
     <>
+      <TitleActionBar
+        title="CMDB Equipos"
+        buttons={
+          <>
+            <button>
+              Licencias Siphone
+            </button>
+            <button>
+              Áreas
+            </button>
+            <button className="blue">
+              Agregar Equipo
+            </button> 
+          </>
+        }
+      />
     </>
   )
 }
