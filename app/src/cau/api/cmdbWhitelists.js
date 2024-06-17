2e5bfb42f9de7275a40f3de2417c472b862dda1e
Carlos Escobedo <carlosdev1810@gmail.com>
2024-06-17 15:46:25 -0600

    whitelists_cmdb: http queries are added to the computers, localEmails and customersEmails API in frontend

--- app/src/cau/api/cmdbWhitelists.js
+++ app/src/cau/api/cmdbWhitelists.js
@@ -12,3 +12,45 @@ export const addZone = async(data) =>
 
 export const updateZone = async(data) =>
   await axios.put(`${url}/zones`, data)
+
+// computers
+
+export const getComputers = async() =>
+  await axios.get(`${url}/computers`)
+
+export const addComputer = async(data) =>
+  await axios.post(`${url}/computers`, data)
+
+export const updateComputer = async(data) =>
+  await axios.put(`${url}/computers`, data)
+
+export const deleteComputer = async(id_computer) =>
+  await axios.delete(`${url}/computers?id_computer=${id_computer}`)
+
+// local emails
+
+export const getLocalEmails = async() =>
+  await axios.get(`${url}/emails/local`)
+
+export const addLocalEmail = async(data) =>
+  await axios.post(`${url}/emails/local`, data)
+
+export const updateLocalEmail = async(data) =>
+  await axios.put(`${url}/emails/local`, data)
+
+export const deleteLocalEmail = async(id_email) =>
+  await axios.delete(`${url}/emails/local?id_email=${id_email}`)
+
+// customers emails
+
+export const getCustomersEmails = async() =>
+  await axios.get(`${url}/emails/customers`)
+
+export const addCustomersEmail = async(data) =>
+  await axios.post(`${url}/emails/customers`, data)
+
+export const updateCustomersEmail = async(data) =>
+  await axios.put(`${url}/emails/customers`, data)
+
+export const deleteCustomersEmail = async(id_email) =>
+  await axios.delete(`${url}/emails/customers?id_email=${id_email}`)
