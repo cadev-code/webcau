0bb331cf96e4efa0613d1dbe41b5a4f0744e3f1a
Carlos Escobedo <carlosescobedomoreno@gmail.com>
2024-07-02 17:43:49 -0600

    directory_cmdb: areas controllers and routes is added in server

--- server/src/controllers/directory_cmdb.controllers.js
+++ server/src/controllers/directory_cmdb.controllers.js
@@ -48,3 +48,52 @@ export const deleteUO = async(req, res) => {
     res.status(400).send('There was an error trying to delete the information.')
   }
 }
+
+// areas
+
+export const getAreas = async(req, res) => {
+  const query = 'SELECT * FROM areas_directory_cmdb ORDER BY `area` ASC'
+
+  try {
+    const [result] = await pool.query(query)
+    res.status(200).json(result)
+  } catch (error) {
+    res.status(400).send('Error when trying to obtain the information.')
+  }
+}
+
+export const addArea = async(req, res) => {
+  const { text } = req.body
+  const query = 'INSERT INTO areas_directory_cmdb (`area`) VALUES (?)'
+
+  try {
+    await pool.query(query, [text])
+    res.status(200).send('Information uploaded correctly.')
+  } catch (error) {
+    res.status(400).send('There was an error trying to load the information.')
+  }
+}
+
+export const updateArea = async(req, res) => {
+  const { id, text } = req.body
+  const query = 'UPDATE areas_directory_cmdb SET `area` =? WHERE id_area = ?'
+
+  try {
+    await pool.query(query, [text, id])
+    res.status(200).send('Information was updated correctly.')
+  } catch (error) {
+    res.status(400).send('There was an error trying to upadate the information.')
+  }
+}
+
+export const deleteArea = async(req, res) => {
+  const { id_area } = req.query
+  const query = 'DELETE FROM areas_directory_cmdb WHERE id_area =?'
+
+  try {
+    await pool.query(query, [id_area])
+    res.status(200).send('Information successfully deleted.')
+  } catch (error) {
+    res.status(400).send('There was an error trying to delete the information.')
+  }
+}
--- server/src/routes/directory_cmdb.routes.js
+++ server/src/routes/directory_cmdb.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { addUO, deleteUO, getUO, updateUO } from '../controllers/directory_cmdb.controllers.js'
+import { addArea, addUO, deleteArea, deleteUO, getAreas, getUO, updateArea, updateUO } from '../controllers/directory_cmdb.controllers.js'
 
 const router = Router()
 
@@ -7,5 +7,10 @@ router.get('/cmdb/directory/uo', getUO)
 router.post('/cmdb/directory/uo', addUO)
 router.put('/cmdb/directory/uo', updateUO)
 router.delete('/cmdb/directory/uo', deleteUO)
+
+router.get('/cmdb/directory/areas', getAreas)
+router.post('/cmdb/directory/areas', addArea)
+router.put('/cmdb/directory/areas', updateArea)
+router.delete('/cmdb/directory/areas', deleteArea)
 
 export default router
