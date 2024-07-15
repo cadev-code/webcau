33f238c4fcf50d90f033a519549c524d85417677
Carlos Alberto <carlosdev1810@gmail.com>
2024-07-15 13:04:24 -0600

    resources_cmdb: TitleActionBar is added in view

--- app/src/cau/views/CMDBResources/index.jsx
+++ app/src/cau/views/CMDBResources/index.jsx
@@ -1,8 +1,24 @@
+import { TitleActionBar } from '../../components'
 
 export const CMDBResources = ({ userData }) => {
   return (
     <>
-    
+      <TitleActionBar 
+        title="CMDB Recursos Compartidos"
+        buttons={
+          <>
+            {
+              // agregar permisos de admin
+              true &&
+                <button className="blue"
+                  onClick={() => {}}
+                >
+                  Agregar Recurso
+                </button>
+            }
+          </>
+        }
+      />
     </>
   )
 }
