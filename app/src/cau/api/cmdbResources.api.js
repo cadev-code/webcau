e6c4df619644b8d9635ae4b19c9a09cf1df2e74f
Carlos Escobedo <carlosescobedomoreno@gmail.com>
2024-07-20 10:56:16 -0600

    resources_cmdb: areas fetch api in app is added

--- app/src/cau/api/cmdbResources.api.js
+++ app/src/cau/api/cmdbResources.api.js
@@ -2,6 +2,22 @@ import axios from 'axios'
 
 const url = `${import.meta.env.VITE_URL_API_WEBCAU_DB}/cmdb/resources`
 
+// areas
+
+export const getAreas = async() =>
+  await axios.get(`${url}/areas`)
+
+export const addArea = async(data) =>
+  await axios.post(`${url}/areas`, data)
+
+export const updateArea = async(data) =>
+  await axios.put(`${url}/areas`, data)
+
+export const deleteArea = async(id) =>
+  await axios.delete(`${url}/areas?id_area=${id}`)
+
+// resources
+
 export const getResources = async() =>
   await axios.get(url)
 
