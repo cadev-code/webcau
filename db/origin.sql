8bca03752e41fec3e0ea62ad0371b3d1d5afe4dd
Carlos Alberto <carlosdev1810@gmail.com>
2024-08-17 19:18:23 -0600

    configuration for deploying with Docker

--- db/origin.sql
+++ db/origin.sql
@@ -1,15 +1,19 @@
-CREATE TABLE origin_computers_cmdb (
-  `id_origin` INT AUTO_INCREMENT,
-  `origin` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
-  PRIMARY KEY (`id_origin`)
-);
+-- CREATE DATABASE IF NOT EXISTS webcau;
 
-INSERT INTO origin_computers_cmdb (`origin`) VALUES 
-  ('Smart'),
-  ('Rentado'),
-  ('Affinitas');
+-- use webcau;
 
-ALTER TABLE cmdb
-ADD `id_origin` int DEFAULT 1,
-ADD CONSTRAINT `fk_relacion3` 
-FOREIGN KEY (`id_origin`) REFERENCES origin_computers_cmdb(`id_origin`) ON DELETE CASCADE;
+-- CREATE TABLE origin_computers_cmdb (
+--   `id_origin` INT AUTO_INCREMENT,
+--   `origin` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+--   PRIMARY KEY (`id_origin`)
+-- );
+
+-- INSERT INTO origin_computers_cmdb (`origin`) VALUES 
+--   ('Smart'),
+--   ('Rentado'),
+--   ('Affinitas');
+
+-- ALTER TABLE cmdb
+-- ADD `id_origin` int DEFAULT 1,
+-- ADD CONSTRAINT `fk_relacion3` 
+-- FOREIGN KEY (`id_origin`) REFERENCES origin_computers_cmdb(`id_origin`) ON DELETE CASCADE;
--- db/webcau_resources_cmdb.sql
+++ db/webcau_resources_cmdb.sql
@@ -1,3 +1,5 @@
+use webcau;
+
 CREATE TABLE areas_resources_cmdb(
   `id_area` INT NOT NULL AUTO_INCREMENT,
   `area` VARCHAR(255) NOT NULL,
