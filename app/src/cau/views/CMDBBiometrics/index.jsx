79e381507b5a50a2cce444ffa94be32aa00935fd
Carlos Alberto <carlosdev1810@gmail.com>
2024-08-21 13:27:15 -0600

    biometrics_cmdb: OptionsManager base added for campaigns

--- app/src/cau/views/CMDBBiometrics/index.jsx
+++ app/src/cau/views/CMDBBiometrics/index.jsx
@@ -1,18 +1,31 @@
+import { useState } from 'react'
 import { TitleActionBar } from '../../components'
+import { OptionsManager } from '../../components/TableData/OptionsManager'
 
 export const CMDBBiometrics = () => {
+
+  const [showCampaigns, setShowCampaigns] = useState(true)
+
   return (
     <>
       <TitleActionBar 
         title="CMDB Biométricos"
         buttons={
           <>
-            <button onClick={() => console.log('campañas')}>
+            <button onClick={() => setShowCampaigns(true)}>
               Campañas
             </button>
           </>
         }
       />
+      {
+        showCampaigns &&
+          <OptionsManager 
+            title="Campañas"
+            close={() => setShowCampaigns(false)}
+            userIsAdmin={true}
+          />
+      }
     </>
   )
 }
