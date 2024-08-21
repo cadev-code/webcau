1edd5c867c345dc07725042b6dd52a97d087803d
Carlos Alberto <carlosdev1810@gmail.com>
2024-08-21 13:05:13 -0600

    biometrics_cmdb: add, update and delete method controllers is added with routes

--- server/src/controllers/biometrics_cmdb.controllers.js
+++ server/src/controllers/biometrics_cmdb.controllers.js
@@ -10,3 +10,39 @@ export const getCampaigns = async(req, res) => {
     res.status(400).send('There was an error to trying obtain the information.')
   }
 }
+
+export const addCampaign = async(req, res) => {
+  const { text } = req.body
+  const query = 'INSERT INTO campaigns_biometrics_cmdb (`campaign`) VALUES (?)'
+
+  try {
+    await pool.query(query, [text])
+    res.status(200).send('Information uploaded correctly.')
+  } catch (error) {
+    res.status(400).send('There was an error to trying upload the information.')
+  }
+}
+
+export const updateCampaign = async(req, res) => {
+  const { id, text } = req.body
+  const query = 'UPDATE campaigns_biometrics_cmdb SET `campaign` = ? WHERE id_campaign = ?'
+
+  try {
+    await pool.query(query, [text, id])
+    res.status(200).send('Information updated correctly.')
+  } catch (error) {
+    res.status(400).send('There was an error to trying update the information.')
+  }
+}
+
+export const deleteCampaign = async(req, res) => {
+  const { id_campaign } = req.query
+  const query = 'DELETE FROM campaigns_biometrics_cmdb WHERE id_campaign = ?'
+
+  try {
+    await pool.query(query, [id_campaign])
+    res.status(200).send('Information deleted correctly.')
+  } catch (error) {
+    res.status(400).send('There was an error to trying delete the information.')
+  }
+}
--- server/src/routes/biometrics_cmdb.routes.js
+++ server/src/routes/biometrics_cmdb.routes.js
@@ -1,8 +1,11 @@
 import { Router } from 'express'
-import { getCampaigns } from '../controllers/biometrics_cmdb.controllers.js'
+import { addCampaign, deleteCampaign, getCampaigns, updateCampaign } from '../controllers/biometrics_cmdb.controllers.js'
 
 const router = Router()
 
 router.get('/cmdb/biometrics/campaign', getCampaigns)
+router.post('/cmdb/biometrics/campaign', addCampaign)
+router.put('/cmdb/biometrics/campaign', updateCampaign)
+router.delete('/cmdb/biometrics/campaign', deleteCampaign)
 
 export default router
