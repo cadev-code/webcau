6554d64d7b2116454bbde6a83c14093dc4c5818e
Carlos Alberto <carlosdev1810@gmail.com>
2024-08-22 10:56:07 -0600

    biometrics_cmdb: mark controllers and routes added

--- server/src/controllers/biometrics_cmdb.controllers.js
+++ server/src/controllers/biometrics_cmdb.controllers.js
@@ -1,5 +1,7 @@
 import { pool } from '../db.js'
 
+// camapigns
+
 export const getCampaigns = async(req, res) => {
   const query = 'SELECT * FROM campaigns_biometrics_cmdb'
 
@@ -41,6 +43,55 @@ export const deleteCampaign = async(req, res) => {
 
   try {
     await pool.query(query, [id_campaign])
+    res.status(200).send('Information deleted correctly.')
+  } catch (error) {
+    res.status(400).send('There was an error to trying delete the information.')
+  }
+}
+
+// marks
+
+export const getMarks = async(req, res) => {
+  const query = 'SELECT * FROM marks_biometrics_cmdb'
+
+  try {
+    const [result] = await pool.query(query)
+    res.status(200).json(result)
+  } catch (error) {
+    res.status(400).send('There was an error to trying obtain the information.')
+  }
+}
+
+export const addMark = async(req, res) => {
+  const { text } = req.body
+  const query = 'INSERT INTO marks_biometrics_cmdb (`mark`) VALUES (?)'
+
+  try {
+    await pool.query(query, [text])
+    res.status(200).send('Information uploaded correctly.')
+  } catch (error) {
+    res.status(400).send('There was an error to trying upload the information.')
+  }
+}
+
+export const updateMark = async(req, res) => {
+  const { id, text } = req.body
+  const query = 'UPDATE marks_biometrics_cmdb SET `mark` = ? WHERE `id_mark` = ?'
+
+  try {
+    await pool.query(query, [text, id])
+    res.status(200).send('Information updated correctly.')
+  } catch (error) {
+    res.status(400).send('There was an error to trying update the information.')
+  }
+}
+
+export const deleteMark = async(req, res) => {
+  const { id_mark } = req.query
+  const query = 'DELETE FROM marks_biometrics_cmdb WHERE `id_mark` = ?'
+
+  try {
+    await pool.query(query, [id_mark])
     res.status(200).send('Information deleted correctly.')
   } catch (error) {
     res.status(400).send('There was an error to trying delete the information.')
--- server/src/routes/biometrics_cmdb.routes.js
+++ server/src/routes/biometrics_cmdb.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { addCampaign, deleteCampaign, getCampaigns, updateCampaign } from '../controllers/biometrics_cmdb.controllers.js'
+import { addCampaign, addMark, deleteCampaign, deleteMark, getCampaigns, getMarks, updateCampaign, updateMark } from '../controllers/biometrics_cmdb.controllers.js'
 
 const router = Router()
 
@@ -7,5 +7,10 @@ router.get('/cmdb/biometrics/campaigns', getCampaigns)
 router.post('/cmdb/biometrics/campaigns', addCampaign)
 router.put('/cmdb/biometrics/campaigns', updateCampaign)
 router.delete('/cmdb/biometrics/campaigns', deleteCampaign)
+
+router.get('/cmdb/biometrics/marks', getMarks)
+router.post('/cmdb/biometrics/marks', addMark)
+router.put('/cmdb/biometrics/marks', updateMark)
+router.delete('/cmdb/biometrics/marks', deleteMark)
 
 export default router
