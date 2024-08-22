7b66946f1adec5895a41876b2437b8e9bac21554
Carlos Alberto <carlosdev1810@gmail.com>
2024-08-22 11:00:10 -0600

    biometrics_cmdb: marks api methods is added in app

--- app/src/cau/api/cmdbBiometrics.api.js
+++ app/src/cau/api/cmdbBiometrics.api.js
@@ -2,6 +2,8 @@ import axios from 'axios'
 
 const URL = `${import.meta.env.VITE_URL_API_WEBCAU_DB}/cmdb/biometrics`
 
+// campaigns
+
 export const getCampaigns = async() =>
   await axios.get(`${URL}/campaigns`)
 
@@ -13,3 +15,17 @@ export const updateCampaign = async(data) =>
 
 export const deleteCampaign = async(id) =>
   await axios.delete(`${URL}/campaigns?id_campaign=${id}`)
+
+// marks
+
+export const getMarks = async() =>
+  await axios.get(`${URL}/marks`)
+
+export const addMark = async(data) =>
+  await axios.post(`${URL}/marks`, data)
+
+export const updateMark = async(data) =>
+  await axios.put(`${URL}/marks`, data)
+
+export const deleteMark = async(id) =>
+  await axios.delete(`${URL}/marks?id_mark=${id}`)
