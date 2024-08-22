9edc9137012a79903f39e937d46e72e4393b8d70
Carlos Alberto <carlosdev1810@gmail.com>
2024-08-22 10:31:47 -0600

    biometrics_cmdb: marks table sql

--- db/webcau_biometrics_cmdb.sql
+++ db/webcau_biometrics_cmdb.sql
@@ -7,3 +7,12 @@ CREATE TABLE campaigns_biometrics_cmdb(
 INSERT INTO campaigns_biometrics_cmdb (`campaign`) VALUES ('SMART CENTER'),
 ('BBVA'),
 ('INFRA');
+
+CREATE TABLE marks_biometrics_cmdb(
+  `id_mark` INT AUTO_INCREMENT NOT NULL,
+  `mark` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id_mark`)
+);
+
+INSERT INTO marks_biometrics_cmdb (`mark`) VALUES ('ZKTeco Inc'),
+('INFRA');
