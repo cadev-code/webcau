06f5226e8fe39d1ac3cfc52a8a826fe8077c84ee
Carlos Alberto <carlosdev1810@gmail.com>
2024-08-30 15:06:01 -0600

    laptops_cmdb: areas table sql

---
+++ db/webcau_laptops_cmdb.sql
@@ -0,0 +1,14 @@
+CREATE TABLE areas_laptops_cmdb(
+  `id_area` INT NOT NULL AUTO_INCREMENT,
+  `area` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id_area`)
+);
+
+INSERT INTO areas_laptops_cmdb (`area`) VALUES ('Capacitación Soluciones de Pago'),
+('Capacitación Soluciones de Pago'),
+('Planeación Estrategia, Mejora Continua y Monitoreo'),
+('Coordinación Generación de la Información'),
+('Capital Humano'),
+('Coordinacion de Nomina'),
+('Gerencia Operativa BBVA Soluciones de Pago'),
+('Gerencia Technology Account Manager');
