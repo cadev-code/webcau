c02c8d10cd8f71c0c09df7d838cd122995c562db
Carlos Alberto <carlosdev1810@gmail.com>
2024-09-24 08:22:11 -0600

    laptops_cmdb: areas optionManager base is added

--- app/src/cau/views/CMDBLaptops/index.jsx
+++ app/src/cau/views/CMDBLaptops/index.jsx
@@ -1,16 +1,30 @@
+import { useState } from 'react'
 import { TitleActionBar } from '../../components'
+import { OptionsManager } from '../../components/TableData'
 
 export const CMDBLaptops = () => {
+
+  const [showAreas, setShowAreas] = useState(true)
+
   return (
     <>
       <TitleActionBar 
         title="CMDB Laptops"
         buttons={
           <>
-            
+            <button onClick={() => setShowAreas(true)}>
+              Áreas
+            </button>
           </>
         }
       />
+      {showAreas && (
+        <OptionsManager
+          title="Áreas"
+          close={() => setShowAreas(false)}
+          userIsAdmin={true}
+        />
+      )}
     </>
   )
 }
