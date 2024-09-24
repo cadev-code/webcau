19ad6bb6c7c72fb1d2190f61c167f716c590d875
Carlos Alberto <carlosdev1810@gmail.com>
2024-09-24 08:33:07 -0600

    laptops_cmdb: add method and refresh areas is added

--- app/src/cau/views/CMDBLaptops/index.jsx
+++ app/src/cau/views/CMDBLaptops/index.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import { TitleActionBar } from '../../components'
 import { OptionsManager } from '../../components/TableData'
 import { laptopsDataRequest } from './laptopsDataRequest'
+import { addArea } from '../../api/cmdbLaptops.api'
 
 export const CMDBLaptops = () => {
 
@@ -33,6 +34,8 @@ export const CMDBLaptops = () => {
         <OptionsManager
           title="Áreas"
           options={areasData.map(({id_area, area}) => ({id: id_area, text: area}))}
+          addOptionMethod={addArea}
+          refreshOptions={getAreasData}
           close={() => setShowAreas(false)}
           userIsAdmin={true}
         />
