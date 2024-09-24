91ea453f4e5e9c5084038bdb47eb5558cb3cc8f7
Carlos Alberto <carlosdev1810@gmail.com>
2024-09-24 09:44:23 -0600

    laptops_cmdb: marks controllers and routes added

--- server/src/controllers/laptops_cmdb.controllers.js
+++ server/src/controllers/laptops_cmdb.controllers.js
@@ -49,3 +49,52 @@ export const deleteArea = async(req, res) => {
     res.status(400).send('There was an error to trying delete the information.')
   }
 }
+
+// marks
+
+export const getMarks = async(req, res) => {
+  const query = 'SELECT * FROM marks_laptops_cmdb'
+
+  try {
+    const [result] = await pool.query(query)
+    res.status(200).json(result)
+  } catch (error) {
+    res.status(400).send('There was an error to trying obtain the information.')
+  }
+}
+
+export const addMark = async(req, res) => {
+  const { text } = req.body
+  const query = 'INSERT INTO marks_laptops_cmdb (`mark`) VALUES (?)'
+  
+  try {
+    await pool.query(query, [text])
+    res.status(200).send('Information uploaded correctly.')
+  } catch(error) {
+    res.status(400).send('There was an error to trying upload the information.')
+  }
+}
+
+export const updateMark = async(req, res) => {
+  const { id, text } = req.body
+  const query = 'UPDATE marks_laptops_cmdb SET `mark` = ? WHERE `id_mark` = ?'
+  
+  try {
+    await pool.query(query, [text, id])
+    res.status(200).send('Information updated correctly.')
+  } catch(error) {
+    res.status(400).send('There was an error to trying update the information.')
+  }
+}
+
+export const deleteMark = async(req, res) => {
+  const { id_mark } = req.query
+  const query = 'DELETE FROM marks_laptops_cmdb WHERE `id_mark` = ?'
+  
+  try {
+    await pool.query(query, [id_mark])
+    res.status(200).send('Information deleted correctly.')
+  } catch(error) {
+    res.status(400).send('There was an error to trying deleted the information.')
+  }
+}
--- server/src/routes/laptops_cmdb.routes.js
+++ server/src/routes/laptops_cmdb.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { addArea, deleteArea, getAreas, updateArea } from '../controllers/laptops_cmdb.controllers.js'
+import { addArea, addMark, deleteArea, deleteMark, getAreas, getMarks, updateArea, updateMark } from '../controllers/laptops_cmdb.controllers.js'
 
 const router = Router()
 
@@ -7,5 +7,10 @@ router.get('/cmdb/laptops/areas', getAreas)
 router.post('/cmdb/laptops/areas', addArea)
 router.put('/cmdb/laptops/areas', updateArea)
 router.delete('/cmdb/laptops/areas', deleteArea)
+
+router.get('/cmdb/laptops/marks', getMarks)
+router.post('/cmdb/laptops/marks', addMark)
+router.put('/cmdb/laptops/marks', updateMark)
+router.delete('/cmdb/laptops/marks', deleteMark)
 
 export default router
