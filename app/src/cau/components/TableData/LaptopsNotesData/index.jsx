d9e29aef6b7e3bd8349dcb0c2d61fe4588ec1904
Carlos Alberto <carlosdev1810@gmail.com>
2024-10-09 20:23:10 -0600

    laptops_cmdb: add and delete notes methods are added

--- app/src/cau/components/TableData/LaptopsNotesData/index.jsx
+++ app/src/cau/components/TableData/LaptopsNotesData/index.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { TextBox } from '../ModalData/styled'
 import { Close } from '@mui/icons-material'
-import { getNotes } from '../../../api/cmdbLaptops.api'
+import { addNote, deleteNote, getNotes } from '../../../api/cmdbLaptops.api'
 
 export const LaptopsNotesData = ({ id_laptop }) => {
 
@@ -17,16 +17,44 @@ export const LaptopsNotesData = ({ id_laptop }) => {
   }, [])
 
   const [showForm, setShowForm] = useState({show: false, mode: ''})
+  const [formValues, setFormValues] = useState({note: '', id_laptop})
+
+  const onChangeInput = ({target}) => 
+    setFormValues({...formValues, note: target.value})
+
+  const closeForm = () => {
+    setShowForm({show: false, mode: ''})
+    setFormValues({note: '', id_laptop})
+  }
+
+  const openForm = (mode, data) => {
+    if(mode === 'add') {
+      setShowForm({show: true, mode: 'add'})
+    } else if('delete') {
+      setShowForm({show: true, mode: 'delete'})
+      setFormValues(data)
+    }
+  }
+
+  const onSubmitForm = async(mode) => {
+    if(mode === 'add') {
+      await addNote(formValues)
+    } else if(mode === 'delete') {
+      await deleteNote(formValues.id_note)
+    }
+    await getNotesData()
+    closeForm()
+  }
 
   return (
     <TextBox className="notes">
       <span>Notas</span>
       {!showForm.show && (
         <div className="notes-container">
-          {notesData.map(({id_note, id_laptop, note}) => (
-            <div key={id_note} className="note">
-              <p>{note}</p>
-              <button onClick={() => setShowForm({show: true, mode: 'delete'})}>
+          {notesData.map((data) => (
+            <div key={data.id_note} className="note">
+              <p>{data.note}</p>
+              <button onClick={() => openForm('delete', data)}>
                 <Close />
               </button>
             </div>
@@ -38,17 +66,27 @@ export const LaptopsNotesData = ({ id_laptop }) => {
           <input 
             type="text" 
             placeholder="Escribe tu nota..."
+            value={formValues.note}
+            onChange={onChangeInput}
+            disabled={showForm.mode === 'delete'}
           />
           <div className="buttons">
-            <button onClick={() => setShowForm({show: false, mode: ''})}>Cancelar</button>
-            <button className={showForm.mode === 'delete' ? 'delete' : ''}>
+            <button
+              onClick={closeForm}
+            >
+                Cancelar
+              </button>
+            <button 
+              className={showForm.mode === 'delete' ? 'delete' : ''}
+              onClick={() => onSubmitForm(showForm.mode)}
+            >
               {showForm.mode === 'add' ? 'Guardar' : 'Eliminar'}
             </button>
           </div>
         </div>
       )}
       {!showForm.show && (
-        <button onClick={() => setShowForm({show: true, mode: 'add'})}>
+        <button onClick={() => openForm('add')}>
           Agregar Nota
         </button>
       )}
