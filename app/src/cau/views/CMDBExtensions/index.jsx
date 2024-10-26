54b12b393ac1993b0419bae4831b60959667b7aa
Carlos Alberto <carlosdev1810@gmail.com>
2024-10-25 19:40:02 -0600

    extensions_cmdb: OptionsManager areas is added

--- app/src/cau/views/CMDBExtensions/index.jsx
+++ app/src/cau/views/CMDBExtensions/index.jsx
@@ -1,16 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { TitleActionBar } from '../../components'
+import { OptionsManager } from '../../components/TableData'
 
 export const CMDBExtensions = () => {
+
+  const [showAreas, setShowAreas] = useState(false)
+
   return (
     <>
       <TitleActionBar 
         title="CMDB Extensiones"
         buttons={
           <>
+            <button
+              onClick={() => setShowAreas(true)}
+            >
+              Areas
+            </button>
           </>
         }
       />
+      {showAreas && (
+        <OptionsManager 
+          title="Áreas"
+          options={[]}
+          addOptionMethod={() => {}}
+          updateOptionMethod={() => {}}
+          deleteOptionMethod={() => {}}
+          refreshOptions={() => {}}
+          close={() => setShowAreas(false)}
+          userIsAdmin={true}
+        />
+      )}
     </>
   )
 }
