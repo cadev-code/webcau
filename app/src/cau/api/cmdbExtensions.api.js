7172a75ec9674162c54db0dc9293d0c32144657b
Carlos Alberto <carlosdev1810@gmail.com>
2024-10-31 19:39:51 -0600

    extensions_cmdb: areas api methods was added in app

---
+++ app/src/cau/api/cmdbExtensions.api.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+const url = `${import.meta.env.VITE_URL_API_WEBCAU_DB}/cmdb/extensions`
+
+export const getAreas = async() =>
+  await axios.get(`${url}/areas`)
+
+export const addArea = async(data) =>
+  await axios.post(`${url}/areas`, data)
+
+export const updateArea = async(data) =>
+  await axios.put(`${url}/areas`, data)
+
+export const deleteArea = async(id) =>
+  await axios.delete(`${url}/areas?id_area=${id}`)
