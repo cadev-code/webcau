99d4e847eff348774145d7396f304c451eb4c389
Carlos Alberto <carlosdev1810@gmail.com>
2024-10-31 21:13:45 -0600

    extensions_cmdb: types api methods was added in app

--- app/src/cau/api/cmdbExtensions.api.js
+++ app/src/cau/api/cmdbExtensions.api.js
@@ -2,6 +2,8 @@ import axios from 'axios'
 
 const url = `${import.meta.env.VITE_URL_API_WEBCAU_DB}/cmdb/extensions`
 
+// areas
+
 export const getAreas = async() =>
   await axios.get(`${url}/areas`)
 
@@ -13,3 +15,17 @@ export const updateArea = async(data) =>
 
 export const deleteArea = async(id) =>
   await axios.delete(`${url}/areas?id_area=${id}`)
+
+// types
+
+export const getTypes = async() =>
+  await axios.get(`${url}/types`)
+
+export const addType = async(data) =>
+  await axios.post(`${url}/types`, data)
+
+export const updateType = async(data) =>
+  await axios.put(`${url}/types`, data)
+
+export const deleteType = async(id) =>
+  await axios.delete(`${url}/types?id_type=${id}`)
