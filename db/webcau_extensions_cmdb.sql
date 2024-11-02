27cc2289328e76dacabef08e0e4fafff7df0f208
Carlos Alberto <carlosdev1810@gmail.com>
2024-11-01 18:10:47 -0600

    extensions_cmdb: extensions table sql

--- db/webcau_extensions_cmdb.sql
+++ db/webcau_extensions_cmdb.sql
@@ -19,3 +19,17 @@ CREATE TABLE sites_extensions_cmdb(
   `site` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
   PRIMARY KEY (`id_site`)
 );
+
+CREATE TABLE extensions_cmdb(
+  `id_extension` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `id_area` INT NOT NULL,
+  `id_type` INT NOT NULL,
+  `extension_number` VARCHAR(255) NOT NULL,
+  `id_site` INT NOT NULL,
+  `ticket` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id_extension`),
+  FOREIGN KEY (`id_area`) REFERENCES areas_extensions_cmdb(`id_area`),
+  FOREIGN KEY (`id_type`) REFERENCES types_extensions_cmdb(`id_type`),
+  FOREIGN KEY (`id_site`) REFERENCES sites_extensions_cmdb(`id_site`)
+);
