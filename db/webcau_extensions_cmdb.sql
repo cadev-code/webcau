b3ecc3264ec35a52eac07a760877252b83db358f
Carlos Alberto <carlosdev1810@gmail.com>
2024-11-05 19:26:06 -0600

    extensions_cmdb: sql file modified

--- db/webcau_extensions_cmdb.sql
+++ db/webcau_extensions_cmdb.sql
@@ -8,17 +8,29 @@ CREATE TABLE areas_extensions_cmdb(
   PRIMARY KEY (`id_area`)
 );
 
+INSERT INTO areas_extensions_cmdb (`area`) VALUES ('Discado'),
+('Control Interno'),
+('Caseta de Vigilancia'),
+('Nomina'),
+('Juridico');
+
 CREATE TABLE types_extensions_cmdb(
   `id_type` INT NOT NULL AUTO_INCREMENT,
   `type` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
   PRIMARY KEY (`id_type`)
 );
 
+INSERT INTO types_extensions_cmdb (`type`) VALUES ('Telefono Fisico'),
+('Avaya');
+
 CREATE TABLE sites_extensions_cmdb(
   `id_site` INT NOT NULL AUTO_INCREMENT,
   `site` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
   PRIMARY KEY (`id_site`)
 );
+
+INSERT INTO sites_extensions_cmdb (`site`) VALUES ('ODA'),
+('La Viga');
 
 CREATE TABLE extensions_cmdb(
   `id_extension` INT NOT NULL AUTO_INCREMENT,
