f02ac7d536c349b5203cb06de23e50744579ac88
Carlos Alberto <carlosdev1810@gmail.com>
2024-11-28 13:47:27 -0600

    FIX whitelists_cmdb: column quantities are corrected from exportData

--- app/src/cau/views/CMDBWhitelists/exportData.js
+++ app/src/cau/views/CMDBWhitelists/exportData.js
@@ -3,17 +3,18 @@ import * as XLSX from 'xlsx'
 export const exportData = (zone, computers, localEmails, customersEmails) => {
   const filename = `${zone}_lista_blanca.xlsx`
   
-  const ws_computers = XLSX.utils.json_to_sheet([])
-  const ws_localEmails = XLSX.utils.json_to_sheet([])
-  const ws_customersEmails = XLSX.utils.json_to_sheet([])
-  const wb = XLSX.utils.book_new()
+  const ws_computers = XLSX.utils.json_to_sheet([]) // hoja nueva
+  const ws_localEmails = XLSX.utils.json_to_sheet([]) // hoja nueva
+  const ws_customersEmails = XLSX.utils.json_to_sheet([]) // hoja nueva
+  const wb = XLSX.utils.book_new() // libro nuevo
 
-  const rows = 30
-  const columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M']
+  const rows = 30 // cantidad de celdas por columna
+  const columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'] // columnas
 
   if(computers.length > rows) {
     const data = computers
-    for (let i = 1; i <= Math.round(data.length / rows); i++) {
+    const columnsAmount = ((data.length / rows)*10)%10 === 0 ? (data.length / rows) : (Math.trunc(data.length / rows) + 1)
+    for (let i = 1; i <= columnsAmount; i++) {
       XLSX.utils.sheet_add_json(ws_computers, data.slice((rows * i - rows), rows * i).map(computer => ({Equipo_NetBIOS: computer.netbios, Equipo_Correo: computer.email})), {origin: `${columns[i * 3 - 3]}1`})
     }
   } else {
@@ -22,7 +23,8 @@ export const exportData = (zone, computers, localEmails, customersEmails) => {
 
   if(localEmails.length > rows) {
     const data = localEmails
-    for (let i = 1; i <= Math.round(data.length / rows); i++) {
+    const columnsAmount = ((data.length / rows)*10)%10 === 0 ? (data.length / rows) : (Math.trunc(data.length / rows) + 1)
+    for (let i = 1; i <= columnsAmount; i++) {
       XLSX.utils.sheet_add_json(ws_localEmails, data.slice((rows * i - rows), rows * i).map(email => ({Correos_Smart_Permitidos: email.email})), {origin: `${columns[i - 1]}1`})
     }
   } else {
@@ -31,7 +33,8 @@ export const exportData = (zone, computers, localEmails, customersEmails) => {
 
   if(customersEmails.length > rows) {
     const data = customersEmails
-    for (let i = 1; i <= Math.round(data.length / rows); i++) {
+    const columnsAmount = ((data.length / rows)*10)%10 === 0 ? (data.length / rows) : (Math.trunc(data.length / rows) + 1)
+    for (let i = 1; i <= columnsAmount; i++) {
       XLSX.utils.sheet_add_json(ws_customersEmails, data.slice((rows * i - rows), rows * i).map(email => ({Correos_Clientes_Permitidos: email.email})), {origin: `${columns[i - 1]}1`})
     }
   } else {
