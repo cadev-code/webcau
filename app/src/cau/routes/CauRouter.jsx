74ff93ee7f5dc5b92862a8f87032ba290e8791c0
Carlos Alberto <carlosdev1810@gmail.com>
2025-04-23 14:01:59 -0600

    feat(app-routes): protected routes (AuthorizedRoute) are added to the modules

--- app/src/cau/routes/CauRouter.jsx
+++ app/src/cau/routes/CauRouter.jsx
@@ -1,12 +1,10 @@
 // react tools imports
-import { Navigate, Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 // components imports
-import { 
-  Menu, 
-  Dashboard 
-} from '../pages'
-import { 
+import { AuthorizedRoute } from './AuthorizedRoute';
+import { Menu, Dashboard } from '../pages';
+import {
   Catalogue,
   CMDB,
   CMDBBiometrics,
@@ -19,41 +17,157 @@ import {
   Maps,
   Office,
   Printers,
-} from '../views'
-import { useSelector } from 'react-redux'
-import { CMDBExtensions } from '../views/CMDBExtensions'
+} from '../views';
+import { useSelector } from 'react-redux';
+import { CMDBExtensions } from '../views/CMDBExtensions';
 
 export const CauRouter = () => {
-
   // User login data
-  const { user } = useSelector(state => state.user)
-  const userData = user[0]
+  const { user } = useSelector(state => state.user);
+  const userData = user[0];
 
   return (
     <Routes>
-      <Route path="menu" element={ <Menu userData={ userData } /> } />
+      <Route
+        path="menu"
+        element={<Menu userData={userData} />}
+      />
 
-      <Route path="main" element={ <Dashboard /> }>
-        <Route path="impresoras" element={ <Printers userData={ userData } /> } />
-        <Route path="catalogo" element={ <Catalogue userData={ userData } /> } />
-        <Route path="office" element={ <Office userData={ userData } /> } />
+      <Route path="main" element={<Dashboard />}>
+        <Route
+          path="mapas"
+          element={<Maps userData={userData} />}
+        />
+
+        <Route
+          path="impresoras"
+          element={
+            <AuthorizedRoute
+              userData={userData}
+              allowedProfile="cau_oda">
+              <Printers userData={userData} />
+            </AuthorizedRoute>
+          }
+        />
+        <Route
+          path="catalogo"
+          element={
+            <AuthorizedRoute
+              userData={userData}
+              allowedProfile="cau_oda">
+              <Catalogue userData={userData} />
+            </AuthorizedRoute>
+          }
+        />
+        <Route
+          path="office"
+          element={
+            <AuthorizedRoute
+              userData={userData}
+              allowedProfile="cau_oda">
+              <Office userData={userData} />
+            </AuthorizedRoute>
+          }
+        />
 
         {/* CMDB */}
-        <Route path="cmdb" element={ <CMDB /> } />
-        <Route path="cmdb/equipos" element={ <CMDBComputers userData={ userData } /> } />
-        <Route path="cmdb/correos" element={ <CMDBEmails userData={ userData } /> } />
-        <Route path="cmdb/listas" element={ <CMDBWhitelists userData={ userData } /> } />
-        <Route path="cmdb/directory" element={ <CMDBDirectory userData={ userData } /> } />
-        <Route path="cmdb/recursos" element={ <CMDBResources userData={ userData } /> }/>
-        <Route path="cmdb/biometricos" element={ <CMDBBiometrics userData={ userData } /> }/>
-        <Route path="cmdb/laptops" element={ <CMDBLaptops userData={ userData } /> }/>
-        <Route path="cmdb/extensiones" element={ <CMDBExtensions userData={ userData } /> }/>
-
-        <Route path="mapas" element={ <Maps userData={ userData } /> } />
+        <Route
+          path="cmdb"
+          element={
+            <AuthorizedRoute
+              userData={userData}
+              allowedProfile="cau_oda">
+              <CMDB />
+            </AuthorizedRoute>
+          }
+        />
+        <Route
+          path="cmdb/equipos"
+          element={
+            <AuthorizedRoute
+              userData={userData}
+              allowedProfile="cau_oda">
+              <CMDBComputers userData={userData} />
+            </AuthorizedRoute>
+          }
+        />
+        <Route
+          path="cmdb/correos"
+          element={
+            <AuthorizedRoute
+              userData={userData}
+              allowedProfile="cau_oda">
+              <CMDBEmails userData={userData} />
+            </AuthorizedRoute>
+          }
+        />
+        <Route
+          path="cmdb/listas"
+          element={
+            <AuthorizedRoute
+              userData={userData}
+              allowedProfile="cau_oda">
+              <CMDBWhitelists userData={userData} />
+            </AuthorizedRoute>
+          }
+        />
+        <Route
+          path="cmdb/directory"
+          element={
+            <AuthorizedRoute
+              userData={userData}
+              allowedProfile="cau_oda">
+              <CMDBDirectory userData={userData} />
+            </AuthorizedRoute>
+          }
+        />
+        <Route
+          path="cmdb/recursos"
+          element={
+            <AuthorizedRoute
+              userData={userData}
+              allowedProfile="cau_oda">
+              <CMDBResources userData={userData} />
+            </AuthorizedRoute>
+          }
+        />
+        <Route
+          path="cmdb/biometricos"
+          element={
+            <AuthorizedRoute
+              userData={userData}
+              allowedProfile="cau_oda">
+              <CMDBBiometrics userData={userData} />
+            </AuthorizedRoute>
+          }
+        />
+        <Route
+          path="cmdb/laptops"
+          element={
+            <AuthorizedRoute
+              userData={userData}
+              allowedProfile="cau_oda">
+              <CMDBLaptops userData={userData} />
+            </AuthorizedRoute>
+          }
+        />
+        <Route
+          path="cmdb/extensiones"
+          element={
+            <AuthorizedRoute
+              userData={userData}
+              allowedProfile="cau_oda">
+              <CMDBExtensions userData={userData} />
+            </AuthorizedRoute>
+          }
+        />
       </Route>
 
-      <Route path="main/" element={ <Navigate to="menu" /> } />
-      <Route path="/*" element={ <Navigate to="menu" /> } />
+      <Route
+        path="main/"
+        element={<Navigate to="menu" />}
+      />
+      <Route path="/*" element={<Navigate to="menu" />} />
     </Routes>
-  )
-}
+  );
+};
