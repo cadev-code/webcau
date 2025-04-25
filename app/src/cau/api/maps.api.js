b43ff5ee396da48c85ae5e3ba7496accb5725aef
Carlos Alberto <carlosdev1810@gmail.com>
2025-04-25 17:25:45 -0600

    feat(app-api): getMaps and getMapOrder API's are modified to receive the value of site as a property

--- app/src/cau/api/maps.api.js
+++ app/src/cau/api/maps.api.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 const url = `${import.meta.env.VITE_URL_API_WEBCAU_DB}/maps`
 
-export const getMaps = async() =>
-  await axios.get(url)
+export const getMaps = async(site) =>
+  await axios.get(`${url}?site=${site}`)
 
 export const uploadMap = async(mapData) =>
   await axios.post(`${url}/upload`, mapData)
@@ -14,8 +14,8 @@ export const updateMap = async(changesData) =>
 export const deleteMap = async({id, path}) =>
   await axios.delete(`${url}/delete?id=${id}&path=${path}`)
 
-export const getMapOrder = async() => 
-  await axios.get(`${url}/order`)
+export const getMapOrder = async(site) => 
+  await axios.get(`${url}/order?site=${site}`)
 
 export const updateMapOrder = async(newOrder) =>
   await axios.put(`${url}/order/update`, newOrder)
