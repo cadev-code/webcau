c9c3e5fa322059f3317b18729fdf6ab4adf5e486
Carlos Alberto <carlosdev1810@gmail.com>
2025-06-21 09:26:23 -0600

    feat(db): add hardening tables

---
+++ db/hardenind_check.sql
@@ -0,0 +1,6 @@
+CREATE TABLE hardening (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  policy_description  VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  checked BOOLEAN NOT NULL,
+  gpo VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
+);
